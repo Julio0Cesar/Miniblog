7e40709e6b35fab752c3b2ccb81313ef34a032bd
Kintiz <juliocesar70777077@gmail.com>
2023-06-17 03:13:14 -0300

    add not found page

--- src/1.Mini-Blog/pages/404/E404.tsx
+++ src/1.Mini-Blog/pages/404/E404.tsx
@@ -2,7 +2,6 @@ import { useLocation } from 'react-router-dom'
 import styles from './E404.module.scss'
 
 const E404 = () => {
-  const location = useLocation()
   
   return (
     <div className={styles.errorPage}>
